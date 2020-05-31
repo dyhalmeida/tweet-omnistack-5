47cc0746358df5c96af6ed0aefc4f67dbd02ea0e
Diego Almeida <diego.almeida@GRUPOCATA.LOCAL>
2020-05-31 12:15:12 -0300

    Added event to create a new tweet

--- frontend/src/Pages/Timeline.js
+++ frontend/src/Pages/Timeline.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import api from '../service/api'
 import twitterLogo from '../twitter.svg'
 import './Timeline.css';
 
 export default class Timeline extends Component {
+    ENTER = 13;
+    state = {
+        newTweet: '',
+    };
+    handleNewTweet = async (e) => {
+        if (e.keyCode !== this.ENTER) return;
+        const content = this.state.newTweet;
+        const author = localStorage.getItem("@tweet:username");
+        await api.post('tweets', { author, content });
+        this.setState({ newTweet: '' });
+    };
+    handleInputChange = (e) => {
+        this.setState({ newTweet: e.target.value });
+    };
     render() {
         return (
+            <div className="timeline-wrapper">
+                <img  height={24} src={twitterLogo} alt="Twitter logo" />
+                <form> 
+                    <textarea value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontecendo?"
+                    />
+                </form>
+            </div>
+
         )
     }
 }
