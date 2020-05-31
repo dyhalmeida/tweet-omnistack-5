ba95c791f4cac1ef3f3699c2d1c9f29c43ad25cb
Diego Almeida <diego.almeida@GRUPOCATA.LOCAL>
2020-05-31 10:52:49 -0300

    Added route to create a like

--- backend/src/routes/TweetRoute.js
+++ backend/src/routes/TweetRoute.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const TweetController = require('../controllers/TweetController')
+const LikeController = require('../controllers/LikeController')
 routes.get('/', (req, res) => res.json({
     status: 'Twitter API Working !'
 }));
@@ -10,4 +11,5 @@ routes.get('/', (req, res) => res.json({
 routes.get("/tweets", TweetController.index);
 routes.get('/tweets/:id', TweetController.show);
 routes.post("/tweets", TweetController.store);
+routes.post("/likes/:id", LikeController.store);
 module.exports = routes;
