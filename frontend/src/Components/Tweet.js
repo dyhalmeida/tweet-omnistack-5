d3aec4e90e687944303a82764902fad5137a1949
Diego Almeida <diego.almeida@GRUPOCATA.LOCAL>
2020-05-31 12:22:29 -0300

    Added like button

--- frontend/src/Components/Tweet.js
+++ frontend/src/Components/Tweet.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import api from '../service/api';
 import like from '../like.svg';
 import './Tweet.css';
 
 export default class Tweet extends Component {
+    
+    handleLike = async (e) =>{
+        const{_id} = this.props.tweet;
+        await api.post(`/likes/${_id}`);
+    };
     render() {
         const { tweet } = this.props;
         return (
             <li className="tweet">
                 <strong>{tweet.author}</strong>
                 <p>{tweet.content}</p>
-                <button type="button" onClick={() => {}}>
+                <button type="button" onClick={this.handleLike}>
                     <img src={like} alt="Like"/>
                     {tweet.likes}
                 </button>
