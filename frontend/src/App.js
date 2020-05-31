314fac5f676489d7d51e9dde999ac9285ac41b31
Diego Almeida <diego.almeida@GRUPOCATA.LOCAL>
2020-05-31 12:10:09 -0300

    Added timeline page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import Timeline from './Pages/Timeline';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Login} />
+          <Route path="/timeline" component={Timeline} />
         </Switch>
       </BrowserRouter>
     );
